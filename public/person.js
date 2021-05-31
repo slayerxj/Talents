65a3037d78c6c1a100b9228099d44d98cd2e6e66
Allen Xiong <allen.xiong@sap.com>
2021-05-31 16:42:04 +0800

    feat: try using material pure css

--- public/person.js
+++ public/person.js
@@ -11,14 +11,14 @@ fetch("/api/person?id=" + s)
     }
     document.getElementById("name").value = a.lastName + firstName;
     document.getElementById("company").value = a.company;
-    document.getElementById("company").onclick =()=> {
+    document.getElementById("company").onclick = () => {
       document.location.href = `/company?id=${a.company}`;
     }
     document.getElementById("email").value = a.email;
-    document.getElementById("email").onclick = ()=> {
-        document.location.href = "mailto:" +a.email;
+    document.getElementById("email").onclick = () => {
+      document.location.href = "mailto:" + a.email;
     }
-    document.getElementById("gender").value = a.gender === "male" ? "男":"女";
+    document.getElementById("gender").value = a.gender === "male" ? "男" : "女";
     document.getElementById("mobile").value = a.mobilePhone;
     document.getElementById("salary").value = a.salary;
   })

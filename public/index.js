849de19d7060a88389810e1389525110d62a99c8
Allen Xiong <allen.xiong@sap.com>
2021-06-02 17:29:47 +0800

    feat: display recent visited in homepage

--- public/index.js
+++ public/index.js
@@ -1,6 +1,21 @@
-// fetch("/content")
-// .then((res) => res.json())
-// .then((data) => {
-//   document.getElementById("mainView").innerText = JSON.stringify(data);
-// })
-// .catch((err) => console.log(err));
+const recentVisited = ["1167", "1166bedd-bfdb-457b-a533-f6e76a103eed", "1168"];
+const table = document.getElementById("recent-tbody");
+recentVisited.forEach((item) => {
+    fetch("/api/person?id=" + item)
+    .then((res) => res.json())
+    .then((data) => {
+      const a = data[0];
+      const row = document.createElement("tr");
+      row.className = "fd-table__row";
+      
+      const content = [a.lastName + a.firstName, a.title, a.companyName, a.salary, (new Date(a.lastModified)).toString()];
+      content.forEach((datum) => {
+        const cell = document.createElement("td");
+        cell.className = "fd-table__cell";
+        cell.innerHTML = datum;
+        row.appendChild(cell);
+      })
+      table.appendChild(row);
+    })
+    .catch((err) => console.log(err));
+})

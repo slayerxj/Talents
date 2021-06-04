d5e3b67516c554cbcd3429af1919116cd1f3706a
Allen Xiong <allen.xiong@sap.com>
2021-06-04 11:16:45 +0800

    refactor: remove unused code and Improve readability fix: fix recent visited persons may duplicated

--- public/person.js
+++ public/person.js
@@ -1,90 +1,67 @@
 const params = new URLSearchParams(document.location.search);
-const s = params.get("id");
+const personId = params.get("id");
 
-if (s) {
-  const asdfa = document.cookie.split("; ").find((row) => row.startsWith("recentVisited="));
-  const recentVisited = asdfa ? asdfa.split("=")[1].split(" ") : [];
+const handleRecentVisitedCookie = () => {
+  const MAX_RECENT_VISITED_LIST_LENGTH = 5;
+  const cookieString = document.cookie.split("; ").find((row) => row.startsWith("recentVisited="));
+  const recentVisited = cookieString ? cookieString.split("=")[1].split(" ") : [];
 
-  if (recentVisited.length > 5) {
-    recentVisited.pop();
+  if (recentVisited.includes(personId)) {
+    const index = recentVisited.indexOf(personId);
+    recentVisited.splice(index, 1);
+  } else {
+    if (recentVisited.length > MAX_RECENT_VISITED_LIST_LENGTH) {
+      recentVisited.pop();
+    }
   }
 
-  recentVisited.unshift(s);
+  recentVisited.unshift(personId);
 
-  document.cookie =
-    "recentVisited=" +
-    recentVisited.join(" ") +
-    "; Expires=" +
-    new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toUTCString();
-  fetch("/api/person?id=" + s)
-    .then((res) => res.json())
-    .then((data) => {
-      const a = data[0];
-      document.getElementById("lastName").value = a.lastName;
-      document.getElementById("firstName").value = a.firstName;
-      document.getElementById("company").value = a.companyName;
-      document.getElementById("company").onclick = () => {
-        document.location.href = `/company?id=${a.company}`;
-      };
-      document.getElementById("email").value = a.email;
-      document.getElementById("email").onclick = () => {
-        document.location.href = "mailto:" + a.email;
-      };
-      document.getElementById("gender").value = a.gender === "male" ? "男" : "女";
-      document.getElementById("mobile").value = a.mobilePhone;
-      document.getElementById("salary").value = a.salary;
-      const c = JSON.parse(a.communication);
-      if (c) {
-        const logs = document.getElementById("communication-log");
-        c.forEach((comment) => {
-          const b = document.createElement("textarea");
-          b.className = "fd-textarea";
-          b.setAttribute("readonly", true);
-          b.value = comment;
-          logs.appendChild(b);
-        });
-      }
+  const oneYearLater = new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toUTCString();
+  document.cookie = `recentVisited=${recentVisited.join(" ")}; Expires=${oneYearLater}`;
+};
 
-      if (a.resume) {
-        const b = document.createElement("a");
-        b.href = "/files/" + a.resume;
-        b.innerText = a.resume;
-        const c = document.getElementById("resume");
-        c.appendChild(b);
-      }
-    })
-    .catch((err) => console.log(err));
-} else {
-  const cells = document.getElementsByTagName("input");
-
-  for (let cell of cells) {
-    cell.removeAttribute("readonly");
-  }
-
-  const applyButton = document.getElementById("apply-person");
-  applyButton.style.visibility = "visible";
-
-  applyButton.onclick = () => {
-    const data = {
-      firstName: document.getElementById("firstName").value,
-      lastName: document.getElementById("lastName").value,
-      mobilePhone: document.getElementById("mobile").value,
-      gender: document.getElementById("gender").value,
-    };
-
-    fetch("/api/persons", {
-      method: "POST", // or 'PUT'
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
+if (!personId) {
+  throw err("No id");
 }
+
+fetch("/api/person?id=" + personId)
+  .then((res) => res.json())
+  .then((data) => {
+    handleRecentVisitedCookie();
+    const a = data[0];
+    document.getElementById("lastName").value = a.lastName;
+    document.getElementById("firstName").value = a.firstName;
+    document.getElementById("company").value = a.companyName;
+    document.getElementById("company").onclick = () => {
+      document.location.href = `/company?id=${a.company}`;
+    };
+    document.getElementById("email").value = a.email;
+    document.getElementById("email").onclick = () => {
+      document.location.href = "mailto:" + a.email;
+    };
+    document.getElementById("gender").value = a.gender === "male" ? "男" : "女";
+    document.getElementById("mobile").value = a.mobilePhone;
+    document.getElementById("salary").value = a.salary;
+
+    const c = JSON.parse(a.communication);
+    if (c) {
+      const logs = document.getElementById("communication-log");
+      c.forEach((comment) => {
+        const b = document.createElement("textarea");
+        b.className = "fd-textarea";
+        b.setAttribute("readonly", true);
+        b.value = comment;
+        logs.appendChild(b);
+      });
+    }
+
+    if (a.resume) {
+      const b = document.createElement("a");
+      b.href = "/files/" + a.resume;
+      b.innerText = a.resume;
+      const c = document.getElementById("resume");
+      c.appendChild(b);
+    }
+  })
+  .catch((err) => console.log(err));
